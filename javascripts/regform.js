45a198284c388b038fd19e004e2531d58487f5bc
RuslanRIS14 <zeliboba123@list.ru>
2016-11-24 19:41:40 +0500

    E-mail

--- javascripts/regform.js
+++ javascripts/regform.js
@@ -25,4 +25,14 @@ function validate(form) {
     } else if (elems.name.value.length < 3 || elems.name.value.length > 20) {
         showError(elems.name.parentNode, ' Имя должно содержать от 3 до 20 букв ');
     }
+
+    resetError(elems.email.parentNode);
+    var email = form.elements.email.value;
+    var regex = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
+
+    if (!email) {
+        showError(elems.email.parentNode, ' Это поле обязательно для заполнения ');
+    } else if (email.search(regex)) {
+        showError(elems.email.parentNode, ' E-mail введен некорректно ');
+    }
 }
